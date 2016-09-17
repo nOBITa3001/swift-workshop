f52d365cb3af333f66a371671ae97d551ec690c4
nOBITa3001 <iampossible3001@gmail.com>
2016-09-17 14:07:00 +0700

    navigation page

---
+++ product/product/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  product
+//
+//  Created by Pongsakorn Thanopassakul on 9/17/2559 BE.
+//  Copyright © 2559 Pongsakorn Thanopassakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
