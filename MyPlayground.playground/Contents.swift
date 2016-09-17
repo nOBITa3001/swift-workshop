adaffc9f55ba7f70514916a8bf52a4b1cb651934
nOBITa3001 <iampossible3001@gmail.com>
2016-09-17 11:41:19 +0700

    my playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name: String = "nOBITa"
+name = "possible"
+print("my name is \(name)")
+
+let 😎 = "cool"
+print(😎)
+
+for character in "nOBITa".characters {
+    print(character)
+}
+
+for character in "🙁🤔😬😁😎".characters {
+    print(character)
+}
+
