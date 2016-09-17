35429086b83c43e4ec53f04c51bd4b84f53b555f
nOBITa3001 <iampossible3001@gmail.com>
2016-09-17 10:13:53 +0700

    hello world

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Pongsakorn Thanopassakul on 9/17/2559 BE.
+//  Copyright © 2559 Pongsakorn Thanopassakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "hi!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
